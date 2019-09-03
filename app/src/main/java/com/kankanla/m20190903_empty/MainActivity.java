77e8104fb7474ece56580e302a5b7afda3a99143
kankanla <koyi_s@hotmail.com>
2019-09-03 10:31:35 +0900

    start_empty

---
+++ app/src/main/java/com/kankanla/m20190903_empty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kankanla.m20190903_empty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
